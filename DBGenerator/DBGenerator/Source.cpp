a2fc8db4d293cbefa83f9cdbde4ec55e83c6ec37
Prosefionol <ilya_kuper@mail.ru>
2022-02-19 17:55:42 +0300

    VS2022 migration

--- DBGenerator/DBGenerator/Source.cpp
+++ DBGenerator/DBGenerator/Source.cpp
@@ -6,19 +6,19 @@ int main(void)
 {
 	int i;
 	double w, w1;
-	FILE*fout = fopen("Output.txt", "w");
+	FILE* fout = fopen("Output.txt", "w");
 	for (i = 0; i < 1000; i++)
 	{
-		const char* supplier[] = { "Kuparenkov","Bazhenov","Isyanova","Iluha","Vovan","Vovchik","Konstantin_Valer'evich","Vladimir_Vladimirovich","Alina",0 };
+		const char* supplier[] = { "Postavchik_A","Postavchik_B","Postavchik_C",0 };
 		int nSupplier = 0;
 		while (supplier[nSupplier]) nSupplier++;
 		fprintf(fout, "%s\n", supplier[rand() % nSupplier]);
-		
+
 		const char* product[] = { "Holodil'nik","Konditsioner","Holodil'naya_vetrina","Lari","Holodil'niy_stol","Vitrina","Perenosnoy_holodil'nik","Ice_generator","Frizer",0 };
 		int nProduct = 0;
 		while (product[nProduct]) nProduct++;
 		fprintf(fout, "%s\n", product[rand() % nProduct]);
-		
+
 		w = double((rand() % 9999 + 1)) / 100;
 		fprintf(fout, "%2.2f ", w);
 		w1 = double((rand() % 4999 + 1)) / 100;
